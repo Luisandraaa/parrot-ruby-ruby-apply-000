1d2a42c78cd520ea43fe51eb1fb1a80650b14b44
luisandra abreu duran <abreuluisandra@gmail.com>
2020-05-19 05:15:42 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(Squawk = "Pretty Bird!")
+def parrot(squawk = "Pretty Bird!")
   puts "Squawk!"
   "Squawk!"
 end
